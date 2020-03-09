95d0aed382c520685ba0ac9cd7aa609f5484114e
cecilioalvarezcaules <cecilioalvarezcaules@10.39.1.65>
2020-03-09 12:31:50 +0100

    codigo inicial

---
+++ 001Basico/src/es/avalon/Principal.java
@@ -0,0 +1,34 @@
+package es.avalon;
+
+public class Principal {
+
+	
+public static void main(String[] args) {
+	
+	byte numero=5; //numero
+	short numero2=600;
+	int numero3=700000;
+	long numero4=100000000;
+	float f1= 10.2323f;
+	double f2=10000.234;
+	char caracter='a';
+	boolean cierto=true;
+	System.out.println("hola desde java");
+
+	System.out.println(numero);
+	numero=8;
+	System.out.println(numero);
+	System.out.println(numero2);
+	System.out.println(numero3);
+	System.out.println(numero4);
+	System.out.println(f1);
+	System.out.println(f2);
+	System.out.println(f2);
+	System.out.println(caracter);
+	System.out.println(cierto);
+	
+	
+	
+}
+
+}
---
+++ 001Basico/src/es/avalon/Principal2.java
@@ -0,0 +1,23 @@
+package es.avalon;
+
+public class Principal2 {
+
+	
+public static void main(String[] args) {
+	
+	double a=5;
+	double b=7;
+	double suma=a+b;
+	double resta=a-b;
+	double multiplicar=a*b;
+	double dividir=a/b;
+	System.out.println(suma);
+	System.out.println(resta);
+	System.out.println(multiplicar);
+	System.out.println(dividir);
+	
+	
+	
+}
+
+}
