a83ea1e021be4c61ff3b7facdadeec21de75d462
Formacion <Formacion@5CD9494ZLHC>
2020-03-10 13:32:40 +0100

    Mejora en ejercio de sumar en Arrays

--- 001Basico/src/es/avalon/Principal6Arrays3.java
+++ 001Basico/src/es/avalon/Principal6Arrays3.java
@@ -7,10 +7,12 @@ public class Principal6Arrays3 {
 
 		int[] a = new int[] { 2, 4, 9 };
 		int[] b = new int[] { 3, 6, 4 };
+		
 
-		System.out.println("La suma de " + a[0] + "+" + b[0] + "=" + (a[0] + b[0]));
-		System.out.println("La suma de " + a[1] + "+" + b[1] + "=" + (a[1] + b[1]));
-		System.out.println("La suma de " + a[2] + "+" + b[2] + "=" + (a[2] + b[2]));
+		for (int i = 0; i < 3; i++) {
+
+			System.out.println(a[i]+b[i]);
+		}
 
 	}
 }
