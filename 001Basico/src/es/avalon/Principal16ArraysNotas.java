1b2a2812f63f0fcdff45663d0fc0fd964bc33016
Formacion <Formacion@5CD9494ZKXC>
2020-03-10 12:51:33 +0100

    array notas

---
+++ 001Basico/src/es/avalon/Principal16ArraysNotas.java
@@ -0,0 +1,22 @@
+package es.avalon;
+
+public class Principal16ArraysNotas {
+
+	public static void main(String[] args) {
+		
+		int [] numeros = new int [] {2,3,4,7,9};
+		int[]  numeros2 = new int [6];
+		
+		System.out.println(numeros [0]);
+		System.out.println(numeros [1]);
+		System.out.println(numeros [2]);
+		System.out.println(numeros [3]);
+		System.out.println(numeros [4]);
+		
+		for(int i=0; i<numeros.length; i++) {
+			System.out.println(numeros[i+1]);
+		}
+
+	}
+
+}
