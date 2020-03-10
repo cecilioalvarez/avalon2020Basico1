3891a8c1df79efc45d3ff66f713f54de831bf66b
Formacion <Formacion@DESKTOP-TRHLJ64>
2020-03-10 11:13:09 +0100

    Actualizacion bucles

--- 001Basico/src/es/avalon/Principal4.java
+++ 001Basico/src/es/avalon/Principal4.java
@@ -8,6 +8,8 @@ public class Principal4 {
 //		for (int i = 0; i<10; i++) {
 //			System.out.println(i);
 //		}
+		
+		
 //  10 PRIMEROS NUMEROS PARES		
 //		for (int i = 0; i<20; i++) {
 //			if (i%2 == 0) {
@@ -15,6 +17,8 @@ public class Principal4 {
 //			i++;
 //			}
 //		}
+		
+		
 //  SUMA 10 PRIMEROS NUMEROS		
 //		int sum = 0;
 //		for (int i = 0; i<10; i++) {
@@ -24,16 +28,70 @@ public class Principal4 {
 //	}
 //		System.out.println(sum);
 		
+		
+//   10 PRIMEROS NUMEROS AL REVES
+//		for (int i = 10; i>0; i--) {
+//
+//			System.out.println(i);
+//
+//	}
+		
 
-		for (int i = 10; i>0; i--) {
+//    10 PRIMEROS NUMEROS CON ESPACIOS	
+//		String espacio = " ";
+//		
+//		for (int i = 0; i<10; i++) {
+//			espacio = espacio + " ";
+//			System.out.println(espacio+i);			
+//	}
+		
+//	  10 PRIMEROS NUMEROS AL CUADRADO		
+//		for (int i = 0; i<10; i++) {
+//			int cuad = i*i;
+//
+//			System.out.println(cuad);			
+//	}		
+		
+//	  CONCATENAR 10 PRIMEROS NUMEROS	
+//		String cadena = "";
+//		for (int i = 0; i<10; i++) {
+//			cadena = cadena + i;
+//
+//			System.out.println(cadena);	
+//					
+//	}	
 
-			System.out.println(i);
+//		  CONCATENAR 10 PRIMEROS NUMEROS (METODO 2 - DOBLE BUCLE)
 
-	}
+//      LISTAR MEDIANTE WHILE
+		
+//		int i = 0;
+//		while (i < 10) {
+//			i = i+1;
+//			System.out.println(i);
+//		}
+//		
+//	}
+	
+////  LISTAR MEDIANTE DO WHILE - terminar
+//		int i = 0;
+//		do {i++;}
+//		while (i < 10);
+//			System.out.println(i);
 		
 		
 		
+//  TABLA DEL 7
+		String cadena = "7";
+		int siete = 7;
+		for (int i = 0; i<10; i++) {
+			int mult = siete * i;
+			String tmp = cadena + "*" + i + "=" + mult;
+
+			System.out.println(tmp);	
+					
+	}			
 		
-		
-	}
 }
+}
+
