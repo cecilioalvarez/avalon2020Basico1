a63a3a719dde74370b333ce1350ed94ddbe3910d
Formacion <Formacion@5CD9494ZLTC>
2020-03-10 14:01:16 +0100

    Suma array final

--- 001Basico/src/es/avalon/Principal15arraysuma.java
+++ 001Basico/src/es/avalon/Principal15arraysuma.java
@@ -5,12 +5,19 @@ public class Principal15arraysuma {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		//Suma de array
-		int [] notas1=new int [] {1,2,3,4,5};
-		int [] notas2=new int [] {5,4,3,2,1};
-		
-		for(int i=0;i<5;i++) {
-			System.out.print(notas1[i]+notas2[i]+",");
+		// Suma de array
+		int[] notas1 = new int[] { 1, 2, 3, 4, 5 };
+		for (int i = 0; i < 5; i++)
+			System.out.print(notas1[i] + ",");
+		System.out.println();
+
+		int[] notas2 = new int[] { 5, 4, 3, 2, 1 };
+		for (int i = 0; i < 5; i++)
+			System.out.print(notas2[i] + ",");
+		System.out.println();
+
+		for (int i = 0; i < 5; i++) {
+			System.out.print(notas1[i] + notas2[i] + ",");
 		}
 	}
 
