18f41652263c0a8f5b12d085984dca859b80d66f
Formacion <Formacion@5CD9494ZLHC>
2020-03-10 13:16:50 +0100

    Ejercico de sumas en Arrays

---
+++ 001Basico/src/es/avalon/Principal6Arrays3.java
@@ -0,0 +1,16 @@
+package es.avalon;
+
+public class Principal6Arrays3 {
+	public static void main(String[] args) {
+
+		// Sumar dos Arrays
+
+		int[] a = new int[] { 2, 4, 9 };
+		int[] b = new int[] { 3, 6, 4 };
+
+		System.out.println("La suma de " + a[0] + "+" + b[0] + "=" + (a[0] + b[0]));
+		System.out.println("La suma de " + a[1] + "+" + b[1] + "=" + (a[1] + b[1]));
+		System.out.println("La suma de " + a[2] + "+" + b[2] + "=" + (a[2] + b[2]));
+
+	}
+}
