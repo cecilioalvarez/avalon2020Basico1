01d9f8cfa10204747274090f90ac93dedc2b37d6
Formacion <Formacion@DESKTOP-E5B1ANP>
2020-03-10 10:00:55 +0000

    ejercicio4

---
+++ 001Basico/src/es/avalon/Principal4.java
@@ -0,0 +1,41 @@
+package es.avalon;
+
+public class Principal4 {
+
+	public static void main(String[] args) {
+		
+		
+
+////		for (int i=0;i<10; i++) {
+////			System.out.println(i*2);
+////			
+////		}
+		for (int i=0;i<20; i++) {
+			
+			if (i%2==0) {
+			System.out.println(i);
+			i++;
+			
+		}
+		int sumarTodos=0;	
+		for (int j=0; j<10;j++) {
+			
+			sumarTodos=j+sumarTodos;
+			
+			System.out.println(sumarTodos);
+		}
+
+		
+		for (int k=10; k>0;k--) {
+			
+			
+			
+			System.out.println(k);
+		}
+		
+		
+		}
+
+	}
+
+}
