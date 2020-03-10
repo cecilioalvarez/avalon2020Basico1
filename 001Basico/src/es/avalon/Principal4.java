1d3614fb9c7b5d88165e1280500a56a0ca2eb837
Formacion <Formacion@DESKTOP-E5B1ANP>
2020-03-10 11:14:26 +0000

    subir

--- 001Basico/src/es/avalon/Principal4.java
+++ 001Basico/src/es/avalon/Principal4.java
@@ -5,37 +5,106 @@ public class Principal4 {
 	public static void main(String[] args) {
 		
 		
-
-////		for (int i=0;i<10; i++) {
-////			System.out.println(i*2);
+//
+//////	for (int i=0;i<10; i++) {
+//////		System.out.println(i*2);
+//////		
+//////	}
+////	for (int i=0;i<20; i++) {
+////		
+////		if (i%2==0) {
+////		System.out.println(i);
+////		i++;
+////		
+////	}
+////	int sumarTodos=0;	
+////	for (int j=0; j<10;j++) {
+////		
+////		sumarTodos=j+sumarTodos;
+////		
+////		System.out.println(sumarTodos);
+////	}
+////
+////		String space=" ";
+////		for (int k=10; k>0;k--) {
 ////			
+////			
+////			String space1=space+" ";
+////			
+////			System.out.println(space1+ k);
 ////		}
-		for (int i=0;i<20; i++) {
-			
-			if (i%2==0) {
-			System.out.println(i);
-			i++;
-			
-		}
-		int sumarTodos=0;	
-		for (int j=0; j<10;j++) {
-			
-			sumarTodos=j+sumarTodos;
-			
-			System.out.println(sumarTodos);
-		}
+//	
+//		String space=" ";
+//		for (int k=0; k<10;k++) {
+//			
+//			
+//			space=space+" ";
+//			
+//			System.out.println(space+k);
+//		}
+//		for (int l=0; l<10;l++) {
+//			
+//			int square=l*l;
+//			
+//			
+//			System.out.println(square);
+//		}
+//		
+//// queremos hacer una concatenacion de numeros , para que ello hay que hacer un bucle anidado
+//		
+//		String conc="";
+//		for(int i=1;i<10;i++) {
+//			
+//			conc=conc+i;
+//			System.out.println(conc);
+//		}
+//		
+//		
+//		
+//		for(int i=0;i<10;i++)
+//		{
+//			for (int j=0;i<i;j++) {
+//				System.out.print(j);
+//			}
+//			System.out.println();
+//		}
+		
+//		int i=0;
+//		
+//		while(i<10) {
+//			i=i+1;
+//			System.out.println(i);
+//			
+//		}
+		
+		//ahora vamos a hacrlo con do-while
+		
+//		int i=0;
+//		
+//		do {
+//			// es importante poner el system out despues de la suma porque 
+//			//sino despues van a poner ceros infinitos
+//			i++;
+//			System.out.println(i);
+//		}
+//		while(i<10);
+	int i=0;
+	String igual ="=";
+	String m ="x";
+	String siete ="7";
+	while(i<10) {
+		i=i+1;
+		int mult=7*i;
+		System.out.println(siete+m+i+igual+mult);
+		
+	
+	}
+		
+ }
 
-		
-		for (int k=10; k>0;k--) {
-			
-			
-			
-			System.out.println(k);
-		}
+	
 		
 		
-		}
-
 	}
 
-}
+
