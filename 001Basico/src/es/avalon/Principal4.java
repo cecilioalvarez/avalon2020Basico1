d62a606556aa20dd446ac41362851e9ffae99203
Formacion <Formacion@5CD9494ZJNC>
2020-03-10 11:06:37 +0100

    hasta las multiplicaciones

--- 001Basico/src/es/avalon/Principal4.java
+++ 001Basico/src/es/avalon/Principal4.java
@@ -3,24 +3,25 @@ package es.avalon;
 public class Principal4 {
 	public static void main (String [] args)
 	{
-		int suma=0;
-		for (int i=0; i<=10; i++) { 
-//			if (i%2==0) {
-//				System.out.println(i);
-//				i++;
-//			}else {
-//				i++;
-//			}
-//			suma= suma+i;
-			int resta= 10-i;
-			System.out.println(resta);
-		}
-		//
-		for (int j=10; j>=0; j--) { 
-
-			System.out.println(j);
-		}
-		
+//		int suma=0;
+//		for (int i=0; i<=10; i++) { 
+////			if (i%2==0) {
+////				System.out.println(i);
+////				i++;
+////			}else {
+////				i++;
+////			}
+////			suma= suma+i;
+//			int resta= 10-i;
+//			System.out.println(resta);
+//		}
+//		//
+//
+//		for (int j=10; j>=0; j--) { 
+//
+//			System.out.println(j);
+//		}
+//		
 //		System.out.println(suma);
 //			
 			
@@ -34,6 +35,53 @@ public class Principal4 {
 //			 * o toggle comment o // para hacer comentarios 
 //			 * 
 //			 */
-		}
+		
+//		String espacio=" "  ;
+//		for (int i=0; i<=10;i++){
+//			espacio= espacio + " ";
+//			
+//			System.out.println(espacio +i);
+//		}
+		
+//		for (int i=0;i<=10;i++) {
+//			System.out.println(i*i);
+//		}
+//		String cadena="";
+//		for (int i=0;i<10;i++) {
+//			cadena= cadena+i;
+//			
+//			System.out.println(cadena);
+//			}
+//			
+//		for (int j=1; j<=10;j++) {
+//			for (int k=1; k<j;k++) {
+//				System.out.print(k);
+//			}
+//			System.out.println();
+//		}
+		int i=0;
+		String cadena =" ";
+	do {
+		cadena= "7 x "+i + " = " ;
+		int multi= 7*i;
+		
+		System.out.println( cadena + multi );
+		i++;
+	}
+	while (i<=10);
+		
+	
+		
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	}
 
+}
