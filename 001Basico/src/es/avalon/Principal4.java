78cf19fbd534435830b7aa7c1b4b47f272c56067
Formacion <Formacion@5CD9494ZKXC>
2020-03-10 09:38:24 +0100

    prueba subida

---
+++ 001Basico/src/es/avalon/Principal4.java
@@ -0,0 +1,19 @@
+package es.avalon;
+
+public class Principal4 {
+
+	public static void main(String[] args) {
+		// esto es un bucle for que calcula los 10 primeros numeros pares
+		for (int i = 0; i < 20; i++) {
+			System.out.println(i++);
+
+			/*
+			 * 
+			 * esto era un bucle
+			 * 
+			 */
+
+		}
+	}
+
+}
