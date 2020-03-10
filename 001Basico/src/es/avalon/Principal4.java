33531a40ba4c425556dc909b76bdd1e270bf65c3
Formacion <Formacion@DESKTOP-TRHLJ64>
2020-03-10 10:03:17 +0100

    Ejercicio4

---
+++ 001Basico/src/es/avalon/Principal4.java
@@ -0,0 +1,39 @@
+package es.avalon;
+
+public class Principal4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//  10 PRIMEROS
+//		for (int i = 0; i<10; i++) {
+//			System.out.println(i);
+//		}
+//  10 PRIMEROS NUMEROS PARES		
+//		for (int i = 0; i<20; i++) {
+//			if (i%2 == 0) {
+//			System.out.println(i);
+//			i++;
+//			}
+//		}
+//  SUMA 10 PRIMEROS NUMEROS		
+//		int sum = 0;
+//		for (int i = 0; i<10; i++) {
+//
+//			sum = sum + i;
+//
+//	}
+//		System.out.println(sum);
+		
+
+		for (int i = 10; i>0; i--) {
+
+			System.out.println(i);
+
+	}
+		
+		
+		
+		
+		
+	}
+}
