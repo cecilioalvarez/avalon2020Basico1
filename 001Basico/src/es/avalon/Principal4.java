1fd912697f0b924cb72a01a750079834b97f014e
Formacion <Formacion@5CD9494ZMXC>
2020-03-10 10:03:15 +0100

    for y sumas 10 marzo

---
+++ 001Basico/src/es/avalon/Principal4.java
@@ -0,0 +1,38 @@
+package es.avalon;
+
+public class Principal4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+	// bucle
+		
+//		for(int i=0; i<10;i++) // i++ = i=i+1
+//			
+//			System.out.println(i*7);
+		
+		// para comentar algo directamente -> selecciono, boton derecho, source, toggle comment
+		
+		
+		/*
+		 * 
+		 * 
+		 * esto era un bucle
+		 */
+		
+		
+		// bucle 10 primeros numero pares // en este te pinta hasta el numero 10
+		
+			for(int i=2; i<10; i=i+2)
+				
+				System.out.println(i);
+			
+		// otra opcion // en este te pinta 10 numeros
+			
+			for(int i=1; i<10; i++)
+				
+				System.out.println(i*2);
+	}
+
+}
---
+++ 001Basico/src/es/avalon/Principal5buclesuma.java
@@ -0,0 +1,22 @@
+package es.avalon;
+
+public class Principal5buclesuma {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//suma de los 10 primeros numeros
+		
+		int suma=0;
+				
+		for(int i=0; i<10; i=i+1) {
+			
+			suma=suma+i;			
+			
+			System.out.println(i);
+			System.out.println(suma); }
+		
+
+	}
+
+}
