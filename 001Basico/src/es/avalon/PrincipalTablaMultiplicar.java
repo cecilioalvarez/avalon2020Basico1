6513dd1e88112731c9ba01abb65c3f567f3d3a15
Formacion <Formacion@5CD9494ZLKC>
2020-03-10 11:06:31 +0100

    Ejercicio tabla de multiplicar 7

---
+++ 001Basico/src/es/avalon/PrincipalTablaMultiplicar.java
@@ -0,0 +1,17 @@
+package es.avalon;
+
+public class PrincipalTablaMultiplicar {
+
+	public static void main(String[] args) {
+
+		int i = 0;
+		int resultado = 0;
+		
+		while (i < 11) {
+			resultado = 7 * i;
+			System.out.println("7 x " + i + " =" + " " +resultado);
+			i++;
+		}
+	}
+}
+
