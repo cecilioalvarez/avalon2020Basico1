68c09408e0298c9fd4797f235c7a8a67a9f8504d
cecilioalvarezcaules <cecilioalvarezcaules@10.39.1.65>
2020-03-10 13:41:29 +0100

    arrays

---
+++ 001Basico/src/es/avalon/Principal11Arrays.java
@@ -0,0 +1,17 @@
+package es.avalon;
+
+public class Principal11Arrays {
+
+	public static void main(String[] args) {
+		
+		int[] lista= new int[] {1,2,3};
+		
+		for (int i=0;i<lista.length;i++) {
+			
+			System.out.println(lista[i]);
+		}
+		
+
+	}
+
+}
