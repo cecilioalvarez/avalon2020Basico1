dc07b9c71d4fb92fd12f2ad833c0a8a539618fa3
Formacion <Formacion@5CD9494ZLHC>
2020-03-10 10:02:33 +0100

    Bucle for y sumas

---
+++ 001Basico/src/es/avalon/Principal4.java
@@ -0,0 +1,20 @@
+package es.avalon;
+
+public class Principal4 {
+
+	public static void main(String[] args) {
+		
+
+		// Esto es un bucle for que cuenta los 10 primeros numeros pares
+		for(int i=1;i<=10;i++) {
+			System.out.println(i*2);
+		}
+		/*
+		 * 
+		 * Esto era un bucle
+		 * 
+		 */
+
+	}
+
+}
---
+++ 001Basico/src/es/avalon/Principal4c.java
@@ -0,0 +1,19 @@
+package es.avalon;
+
+public class Principal4c {
+
+	public static void main(String[] args) {
+
+		int sumar = 0;
+
+		// Esto es un bucle for que suma los primeros 10 numeros
+		for(int i=1;i<=10;i++) {
+			System.out.println(i);
+		}
+
+		System.out.println(sumar);
+
+		
+	}
+
+}
