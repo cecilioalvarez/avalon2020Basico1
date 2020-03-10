0969c5a5039b8eb5aef2724ab8e8bcc8186679eb
Formacion <Formacion@5CD9494ZLTC>
2020-03-10 10:05:33 +0100

    Actualizacion nombres

--- 001Basico/src/es/avalon/Principal6bucleinverso.java
+++ 001Basico/src/es/avalon/Principal6bucleinverso.java
@@ -1,12 +1,12 @@
 package es.avalon;
 
-public class Principal6 {
+public class Principal6bucleinverso {
 
 	public static void main(String[] args) {
 		
 		//Muestro numeros de 10 a 1
 		
-		for(int i=10;i>0;i=i-1) {
+		for(int i=10;i>0;i=i-1) { // i--=>i=i-1
 			
 			System.out.println(i);
 			
