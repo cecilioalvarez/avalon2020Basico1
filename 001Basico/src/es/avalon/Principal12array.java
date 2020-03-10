84ecb814d5c5f434041a2383fc6191895f279d39
Formacion <Formacion@5CD9494ZMXC>
2020-03-10 12:11:56 +0100

    array 10 marzo

---
+++ 001Basico/src/es/avalon/Principal12array.java
@@ -0,0 +1,22 @@
+package es.avalon;
+
+public class Principal12array {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		// array
+		
+		int [] numeros=new int [] {1,2,3};
+		
+		System.out.println(numeros[0]);
+		System.out.println(numeros[1]);
+		System.out.println(numeros[2]);
+		
+		for (int i=0; i<numeros.length; i++) {
+			
+			System.out.print(numeros[i]);
+		}
+	}
+
+}
