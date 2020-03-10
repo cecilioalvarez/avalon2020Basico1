3047c071149df621905de5687f69a6b4af1264ae
Formacion <Formacion@5CD9494ZJNC>
2020-03-10 10:06:35 +0100

    ejercicios

---
+++ 001Basico/src/es/avalon/Principal4.java
@@ -0,0 +1,39 @@
+package es.avalon;
+
+public class Principal4 {
+	public static void main (String [] args)
+	{
+		int suma=0;
+		for (int i=0; i<=10; i++) { 
+//			if (i%2==0) {
+//				System.out.println(i);
+//				i++;
+//			}else {
+//				i++;
+//			}
+//			suma= suma+i;
+			int resta= 10-i;
+			System.out.println(resta);
+		}
+		//
+		for (int j=10; j>=0; j--) { 
+
+			System.out.println(j);
+		}
+		
+//		System.out.println(suma);
+//			
+			
+			
+//			/* lista de los primeros 10 numeros pares
+//			 * 
+//			 */
+//			
+//			System.out.println(i*2);
+//			/*
+//			 * o toggle comment o // para hacer comentarios 
+//			 * 
+//			 */
+		}
+	}
+
