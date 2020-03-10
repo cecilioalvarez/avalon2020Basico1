9dc9332d735e7234e5b5b6f94b16452116dd967a
Formacion <Formacion@DESKTOP-E5B1ANP>
2020-03-10 12:51:43 +0000

    a

--- 001Basico/src/es/avalon/Principal5.java
+++ 001Basico/src/es/avalon/Principal5.java
@@ -1,10 +1,28 @@
 package es.avalon;
 
 public class Principal5 {
-
+//Vamos a hacer arrays
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+//		int[] numeros=new int[] {1,2,3};
+//		
+//		System.out.println(numeros[0]);
+//		System.out.println(numeros[1]);
+//		System.out.println(numeros[2]);
+//		
+//		for(int i=0; i<numeros.length; i++) {
+//			System.out.print(numeros[i]);
+//		}
+		int[] notas=new int[] {1,5,2,4,6};
+		
+		for(int i=0; i<notas.length;i++) {
+			if (notas[i]<5) {
+				notas[i]=notas[i]+1;
+			}
+			System.out.print(notas[i]);
+		}
+	
+		
+		}
 	}
 
-}
+
