77d2a86139ff2d598da65fe8932c7991cad6e187
Formacion <Formacion@DESKTOP-TRHLJ64>
2020-03-10 13:23:15 +0100

    Actualizacion principal5

--- 001Basico/src/es/avalon/Principal5.java
+++ 001Basico/src/es/avalon/Principal5.java
@@ -26,15 +26,22 @@ public class Principal5 {
 //				System.out.println(notas[i]);
 //			}
 //		}
-		int[] notas = new int[] {2,3,4,6,8};
-		
-		for (int i=0; i<notas.length; i++) {
-			if (notas[i] < 5) {
-				System.out.println(notas[i]+1);
-			}
-			else {
-				System.out.println(notas[i]);
-			}
+//		int[] notas = new int[] {2,3,4,6,8};
+//		
+//		for (int i=0; i<notas.length; i++) {
+//			if (notas[i] < 5) {
+//				System.out.println(notas[i]+1);
+//			}
+//			else {
+//				System.out.println(notas[i]);
+//			}
+//		}
+		int[] arr1 = new int[] {2,4,6,8,10};
+		int[] arr2 = new int[] {1,3,5,7,9};
+		int[] arrSum = new int[] {0,0,0,0,0};
+		for (int i=0; i<arr1.length; i++) {
+			arrSum[i] = arr1[i]+arr2[i];
+			System.out.println(arrSum[i]);
 		}
 
 	}
