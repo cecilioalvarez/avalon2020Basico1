8a2ff097b0c1a9b8b3dabfac5b46e563f392889f
Formacion <Formacion@5CD9494ZLTC>
2020-03-10 10:02:46 +0100

    Bucle for y sumas 10 de Marzo

---
+++ 001Basico/src/es/avalon/Principal6.java
@@ -0,0 +1,17 @@
+package es.avalon;
+
+public class Principal6 {
+
+	public static void main(String[] args) {
+		
+		//Muestro numeros de 10 a 1
+		
+		for(int i=10;i>0;i=i-1) {
+			
+			System.out.println(i);
+			
+		}
+
+	}
+
+}
