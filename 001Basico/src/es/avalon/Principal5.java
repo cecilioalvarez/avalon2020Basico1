55f434ec1734576bf2d4ea64ea5e94f2c16a6270
Formacion <Formacion@DESKTOP-E5B1ANP>
2020-03-10 13:50:22 +0000

    revision cecilio

--- 001Basico/src/es/avalon/Principal5.java
+++ 001Basico/src/es/avalon/Principal5.java
@@ -12,17 +12,49 @@ public class Principal5 {
 //		for(int i=0; i<numeros.length; i++) {
 //			System.out.print(numeros[i]);
 //		}
-		int[] notas=new int[] {1,5,2,4,6};
-		
-		for(int i=0; i<notas.length;i++) {
-			if (notas[i]<5) {
-				notas[i]=notas[i]+1;
+//		int[] notas=new int[] {1,5,2,4,6};
+//		
+//		for(int i=0; i<notas.length;i++) {
+//			if (notas[i]<5) {
+//				notas[i]=notas[i]+1;
+//			}
+//			System.out.print(notas[i]);
+//		}//otra forma es con un for
+		// for(int i=0;i<numeros.length;i++) { syso(numeros)
+
+		int[] Array1 = new int[] { 1, 2, 3, 4, 5 };
+		int[] Array2 = new int[] { 1, 2, 3, 4, 5 };
+		int[] Array3 = new int[5];
+
+		for (int i = 0; i < Array1.length; i++) {
+
+			Array3[i] = Array1[i] + Array2[i];
+
+			if (i < Array1.length - 1) {
+				System.out.print(Array3[i] + ",");
+			} else {
+				System.out.println(Array3[i]);
 			}
-			System.out.print(notas[i]);
-		}
-	
-		
 		}
 	}
+
+}
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
