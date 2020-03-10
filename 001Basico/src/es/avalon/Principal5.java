91689a9664b3fb600434825c96e77c23ca07aa28
Formacion <Formacion@5CD9494ZJNC>
2020-03-10 11:36:06 +0100

    arrays

---
+++ 001Basico/src/es/avalon/Principal5.java
@@ -0,0 +1,16 @@
+package es.avalon;
+
+public class Principal5 {
+	public static void main (String [] args)
+	{
+		int[]numeros= new int [] {1,2,3};
+		System.out.println(numeros);
+	System.out.println(numeros [0]);
+	System.out.println(numeros [1]);
+	System.out.println(numeros [2]);
+	
+	for(int i= 0; i<numeros.length; i++)
+	System.out.println(numeros[i]);
+	}
+
+}
