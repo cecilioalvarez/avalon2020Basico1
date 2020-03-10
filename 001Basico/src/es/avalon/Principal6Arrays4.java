67f6d392d21469e61b1031f1879581f1e05ab495
Formacion <Formacion@5CD9494ZLHC>
2020-03-10 14:00:47 +0100

    Ejercicios ARRAYS

--- 001Basico/src/es/avalon/Principal6Arrays4.java
+++ 001Basico/src/es/avalon/Principal6Arrays4.java
@@ -1,18 +1,20 @@
 package es.avalon;
 
-public class Principal6Arrays3 {
+public class Principal6Arrays4 {
 	public static void main(String[] args) {
 
-		// Sumar dos Arrays
+		// Array que escoge el numero mayor
 
 		int[] n1 = new int[] { 2, 4, 9 };
 		int[] n2 = new int[] { 4, 6, 4 };
 		
+		
 		int[] n3 = new int[3];
 
 		for (int i = 0; i < n1.length; i++) {
+			n3 = n1[0];
 
-			n3[i] = n1[i] + n2[i];
+			
 		}
 		
 		for (int j = 0; j < n3.length; j++) {
