bf036d71300d0ba8998247cb84a0d0b9d9a12f29
Formacion <Formacion@5CD9494ZLHC>
2020-03-10 13:33:51 +0100

    Cambio en variables

--- 001Basico/src/es/avalon/Principal6Arrays3.java
+++ 001Basico/src/es/avalon/Principal6Arrays3.java
@@ -5,13 +5,13 @@ public class Principal6Arrays3 {
 
 		// Sumar dos Arrays
 
-		int[] a = new int[] { 2, 4, 9 };
-		int[] b = new int[] { 3, 6, 4 };
+		int[] n1 = new int[] { 2, 4, 9 };
+		int[] n2 = new int[] { 3, 6, 4 };
 		
 
 		for (int i = 0; i < 3; i++) {
 
-			System.out.println(a[i]+b[i]);
+			System.out.println(n1[i]+n2[i]);
 		}
 
 	}
