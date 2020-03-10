eaee553eb1ecdb4c62732b451bea8d59b74502cc
cecilioalvarezcaules <cecilioalvarezcaules@10.39.1.65>
2020-03-10 12:56:34 +0100

    cambios del profe

--- 001Basico/src/es/avalon/Principal16ArraysNotas.java
+++ 001Basico/src/es/avalon/Principal16ArraysNotas.java
@@ -5,16 +5,20 @@ public class Principal16ArraysNotas {
 	public static void main(String[] args) {
 		
 		int [] numeros = new int [] {2,3,4,7,9};
-		int[]  numeros2 = new int [6];
 		
-		System.out.println(numeros [0]);
-		System.out.println(numeros [1]);
-		System.out.println(numeros [2]);
-		System.out.println(numeros [3]);
-		System.out.println(numeros [4]);
+	
 		
 		for(int i=0; i<numeros.length; i++) {
-			System.out.println(numeros[i+1]);
+			
+			
+			if (numeros[i]<5) {
+				numeros[i]=numeros[i]+1;
+			}
+		}
+		
+		for (int i=0;i<numeros.length;i++) {
+			
+			System.out.println(numeros[i]);
 		}
 
 	}
