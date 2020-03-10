fedf1750dbf260836a082875e2a16291c447ed34
Formacion <Formacion@5CD9494ZLKC>
2020-03-10 13:54:32 +0100

    Ejercicio Coger el mayor

---
+++ 001Basico/src/es/avalon/PrincipalArraysCogerMayor.java
@@ -0,0 +1,28 @@
+package es.avalon;
+
+public class PrincipalArraysCogerMayor {
+
+	public static void main(String[] args) {
+
+		int[] array1 = new int[] { 1, 2, 3, 4, 5 };
+		int[] array2 = new int[] { 0, 1, 7, 3, 9 };
+		int[] array3 = new int[array1.length];
+
+		for (int i = 0; i < array1.length; i++) {
+			System.out.print(array1[i] + " ");
+		}
+		System.out.println("  --> Array 1");
+		for (int i = 0; i < array1.length; i++) {
+			System.out.print(array2[i] + " ");
+		}
+		System.out.println("  --> Array 2");
+		for (int i = 0; i < array1.length; i++) {
+			array3[i] = array2[i];
+			if (array2[i]<array1[i]) {
+				array3[i]=array1[i];
+			}
+			System.out.print(array3[i] + " ");
+		}
+		System.out.println(" --> Array Mayor");
+	}
+}
