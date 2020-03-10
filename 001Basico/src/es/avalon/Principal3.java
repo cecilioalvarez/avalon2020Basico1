d0794633f48abaf22d322e4bceb684df00b7c940
Formacion <Formacion@5CD9494ZLHC>
2020-03-10 09:12:02 +0100

    Ctrl+Shift+F

--- 001Basico/src/es/avalon/Principal3.java
+++ 001Basico/src/es/avalon/Principal3.java
@@ -6,27 +6,27 @@ public class Principal3 {
 
 		int numero = 7;
 
-		if(numero < 5) {
-			 
-            System.out.println("Suspenso");
- 
-        }else if( numero < 5) {
- 
-            System.out.println("Suficiente");
- 
-        }else if( numero < 7) {
- 
-            System.out.println("Bien");
- 
-        }else if (numero < 9) {
- 
-            System.out.println("Notable");
- 
-        }else {
- 
-            System.out.println("Sobresaliente");
- 
-        }
+		if (numero < 5) {
+
+			System.out.println("Suspenso");
+
+		} else if (numero < 5) {
+
+			System.out.println("Suficiente");
+
+		} else if (numero < 7) {
+
+			System.out.println("Bien");
+
+		} else if (numero < 9) {
+
+			System.out.println("Notable");
+
+		} else {
+
+			System.out.println("Sobresaliente");
+
+		}
 
 	}
 
--- 001Basico/src/es/avalon/Principal3b.java
+++ 001Basico/src/es/avalon/Principal3b.java
@@ -3,38 +3,37 @@ package es.avalon;
 import java.util.Scanner;
 
 public class Principal3b {
-	
+
 	static Scanner sc = new Scanner(System.in);
 
 	public static void main(String[] args) {
-		
+
 		int numero;
-	       
-        System.out.println("Tio, ¿que nota sacaste en el examen?");
-        numero=sc.nextInt();
-       
-       
-        if(numero < 5) {
-			 
-            System.out.println("Que horror");
- 
-        }else if( numero < 5) {
- 
-            System.out.println("Por los pelos");
- 
-        }else if( numero < 7) {
- 
-            System.out.println("Ni tan mal");
- 
-        }else if (numero < 9) {
- 
-            System.out.println("Nada mal bro");
- 
-        }else {
- 
-            System.out.println("OLE OLE Notaza");
- 
-        }
+
+		System.out.println("Tio, ¿que nota sacaste en el examen?");
+		numero = sc.nextInt();
+
+		if (numero < 5) {
+
+			System.out.println("Que horror");
+
+		} else if (numero < 5) {
+
+			System.out.println("Por los pelos");
+
+		} else if (numero < 7) {
+
+			System.out.println("Ni tan mal");
+
+		} else if (numero < 9) {
+
+			System.out.println("Nada mal bro");
+
+		} else {
+
+			System.out.println("OLE OLE Notaza");
+
+		}
 
 	}
 
