1efea013298a7174bd3308135d652cb6c991ea32
Formacion <Formacion@5CD9494ZLKC>
2020-03-10 10:57:53 +0100

    Ejercicio Do{} While();

---
+++ 001Basico/src/es/avalon/PrincipalSumaWhile.java
@@ -0,0 +1,21 @@
+package es.avalon;
+
+public class PrincipalSumaWhile {
+
+	public static void main(String[] args) {
+
+		int i = 0;
+		while (i < 10) {
+			System.out.println(i);
+			i++;
+		}
+
+		int j=10;
+		do {
+			System.out.println(j);
+			j++;
+		} while (j <= 20);
+
+	}
+}
+
