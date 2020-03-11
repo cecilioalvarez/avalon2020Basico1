9ca2bb9356703d43067ae0f18e5bad5921ef036f
Formacion <Formacion@DESKTOP-E5B1ANP>
2020-03-11 12:25:05 +0000

    otros

--- 001Basico/src/es/avalon/Principal13Funciones.java
+++ 001Basico/src/es/avalon/Principal13Funciones.java
@@ -2,25 +2,30 @@ package es.avalon;
 
 public class Principal13Funciones {
 
-	private static void lista(int a) {
-
-		int[] Array3 = new int[a];
-		for (int i = 0; i < a; i++) {
-			Array3[i] = i;
-			System.out.println(Array3[i]);
-		}
-
-	}
+//	private static void lista(int a) {
+//
+//		int[] Array3 = new int[a];
+//		for (int i = 0; i < a; i++) {
+//			Array3[i] = i;
+//			System.out.println(Array3[i]);
+//		}
+//
+//	}
 
 	public static void main(String[] args) {
 
 //		
 		// lista(3);
 
-
 //		hola();
 //		Adios();		
 //		hola();
+		
+		int[] lista=new int[] {1,3,5,6};
+		
+		
+		System.out.println(Suma(lista));
+		
 //
 	}
 //
@@ -48,9 +53,27 @@ public class Principal13Funciones {
 //			// System.out.println(suma);
 //		}
 //		return sum;
+	
+
+//	private static int Mayor(int[] unaLista) {
+//		int mayor = 0;
+//		for (int i = 0; i <unaLista.length; i++) {
+//			if (unaLista[i] > mayor) {
+//				mayor=unaLista[i];
+//			}
+//		}
+//		return mayor;
 //	}
-
-
+  // una funcion que me recorra el array y me sume todos los valores mayores que 3
+	private static int Suma(int[] unaLista) {
+		int inicial = 0;
+		for (int i = 0; i <unaLista.length; i++) {
+			if (unaLista[i] > 3) {
+				inicial=inicial+unaLista[i];
+			}
 	}
+		return inicial;
+	}
+}
 
 
