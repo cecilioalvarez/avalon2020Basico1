d5b29c263dc3838871f2f896dfe7cc493c6bb7cc
Formacion <Formacion@5CD9494ZLTC>
2020-03-11 12:05:40 +0100

    Funciones con array

---
+++ 001Basico/src/es/avalon/Principal21arraymayorvalor.java
@@ -0,0 +1,33 @@
+package es.avalon;
+
+public class Principal21arraymayorvalor {
+
+	public static void main(String[] args) {
+		
+		// uncion que escoge el mayor valor de un array
+
+		int[] vector=new int[] {7,25,3,1,8}; //introduzco array
+		
+		int resultado=valormayor(vector);
+		System.out.println(resultado);
+	}	
+		private static int valormayor(int[] vector) {
+			int mayor=0;
+			
+			for(int i=0;i<vector.length;i++) {
+				
+				if (vector[i]>mayor) {
+				mayor=vector[i];
+				}
+				else {
+				mayor=mayor;
+				}
+			}
+			return mayor;
+		}
+				
+				
+				
+		
+	}
+
