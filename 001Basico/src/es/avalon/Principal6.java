44ffad1a26fbac742b9894430f79ca22cc99df3c
Formacion <Formacion@5CD9494ZJNC>
2020-03-11 09:26:36 +0100

    metodo lista

---
+++ 001Basico/src/es/avalon/Principal6.java
@@ -0,0 +1,57 @@
+package es.avalon;
+
+public class Principal6 {
+	public static void main (String [] args)
+	{
+		
+		lista(7);
+		
+		
+		
+		
+			
+			
+		}
+	private static int lista (int a) {
+		for (int i>=0; i<=a;i++) {
+			System.out.println(i);
+		}
+	}
+}
+	
+		
+		
+		
+		
+		
+		
+		
+		//hola();
+//		int resultado=suma (5,6);
+//		System.out.println(resultado);
+//		
+//		
+		
+		//adios();
+		
+		
+		
+//	}
+//	private static int suma(int a , int b) {
+//		return a+b;
+//	}
+//	private static void adios() {
+//		System.out.println("adios");
+//		System.out.println("adios");
+//		System.out.println("adios");
+//		System.out.println("adios");
+//	}
+
+//	private static void hola() {
+//		System.out.println("hola");
+//		System.out.println("hola");
+//		System.out.println("hola");
+//		System.out.println("hola");
+//	}
+//	
+//}
