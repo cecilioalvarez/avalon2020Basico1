86b872915b8c311e68f454bc95b8e060fc0b5603
Formacion <Formacion@5CD9494ZJNC>
2020-03-11 13:16:55 +0100

    yata

--- 001Basico/src/es/avalon/Principal6.java
+++ 001Basico/src/es/avalon/Principal6.java
@@ -7,8 +7,12 @@ public class Principal6 {
 //		int [] unalista= new int [] {1,2,3,85};
 //		//eligira el numero más alto
 //		System.out.println(masalto(unalista));
-		int [] unalista= new int [] {1,2,3,26,0,85}; // que me sume todo lo de la lista que sea mayor que 3
-		System.out.println(sumamasalto(unalista));
+		int [] unalista= new int [] {1,12,3,26,0,85}; // que me sume todo lo de la lista que sea mayor que 3
+		int[]  nueva=backward(unalista);
+		
+		for (int j = 0; j < nueva.length; j++) {
+			System.out.println(nueva[j]);
+		}
 		
 //		lista(7); // hace una lista desde el cero hasta el numero pedido
 		
@@ -21,19 +25,38 @@ public class Principal6 {
 //		
 	}
 	
-	private static int sumamasalto(int[] numeros ) {
-		int a=3;
-		int suma=0;
-		for (int i=1; i<numeros.length;i++) {
-			if (numeros[i]>a) {
-				suma = suma + numeros[i];
-				i++;
-			}else {
-				i++;
-			}
-		}
-		return suma;
+	private static int [] backward(int[] numeros ) {
+		int [] nueva= new int [numeros.length];
+		int L= numeros.length;
+		for (int i=0; i< numeros.length;i++) {
+			nueva [L-i-1]= numeros [i];
+		}return nueva;
 	}
+	//otra version
+	/*
+	 * 
+	 */
+}
+	
+	
+	
+	
+	//sumar los numero mas altos que el 3 
+	
+	
+//	private static int sumamasalto(int[] numeros ) {
+//		int a=3;
+//		int suma=0;
+//		for (int i=1; i<numeros.length;i++) {
+//			if (numeros[i]>a) {
+//				suma = suma + numeros[i];
+//				i++;
+//			}else {
+//				i++;
+//			}
+//		}
+//		return suma;
+//	}
 	
 	
 //	private static int [] inferno (int[] numeros ) {
@@ -78,7 +101,7 @@ public class Principal6 {
 		
 //	}
 	
-}
+//}
 	
 		
 		
