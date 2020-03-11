f9060641c1711e5ce5d8a5b80afc83a2d81633b4
Formacion <Formacion@DESKTOP-E5B1ANP>
2020-03-11 11:03:45 +0000

    a

--- 001Basico/src/es/avalon/Principal13Funciones.java
+++ 001Basico/src/es/avalon/Principal13Funciones.java
@@ -1,27 +1,23 @@
 package es.avalon;
 
 public class Principal13Funciones {
-	
-	
-	private static void lista (int a) {
-		
+
+	private static void lista(int a) {
+
 		int[] Array3 = new int[a];
-		for (int i=0; i<a; i++)
-		{
-			Array3[i]=i;
+		for (int i = 0; i < a; i++) {
+			Array3[i] = i;
 			System.out.println(Array3[i]);
 		}
-		
-		
+
 	}
-	
-	
-	
+
 	public static void main(String[] args) {
-		
+
 //		
-		lista(3);
-		
+		// lista(3);
+
+
 //		hola();
 //		Adios();		
 //		hola();
@@ -42,10 +38,19 @@ public class Principal13Funciones {
 //		System.out.println("hola");
 //	}
 
-	
+	// ahora quiero una funcion que me devuelva hasta la suma de ese numero
 
-	
+//	private static int Sumita(int a) {
+//
+//		int sum = 0;
+//		for (int i = 0; i < a; i++) {
+//			sum = sum + i;
+//			// System.out.println(suma);
+//		}
+//		return sum;
+//	}
+
 
 	}
-	
+
 
