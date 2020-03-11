7a23d9993df1a0b5bfbd7ead4793e17c977f7437
Formacion <Formacion@5CD9494ZJNC>
2020-03-11 09:40:31 +0100

    metodos hasta suma

--- 001Basico/src/es/avalon/Principal6.java
+++ 001Basico/src/es/avalon/Principal6.java
@@ -4,21 +4,32 @@ public class Principal6 {
 	public static void main (String [] args)
 	{
 		
-		lista(7);
+//		lista(7); // hace una lista desde el cero hasta el numero pedido
 		
-		
-		
-		
-			
+		suma (10); // suma todos los mnumero entero hasta el numero pedido
 			
 		}
-	private static void lista (int a) {
-		
+	// metodo lista en el que haces una suma de 0 hasta el numero pedido
+	private static void suma (int a) {
+		int suma=0;
 		for (int i=0; i<=a;i++) {
-			System.out.println(i);
+			suma=suma+i;
+					
+			
 		}
+		System.out.println(suma);
+		
 		
 	}
+	// metodo lista en el que haces una lista de 0 hasta el numero pedido
+//	private static void lista (int a) {
+//		
+//		for (int i=0; i<=a;i++) {
+//			System.out.println(i);
+//		}
+		
+//	}
+	
 }
 	
 		
@@ -28,27 +39,29 @@ public class Principal6 {
 		
 		
 		
-		//hola();
-//		int resultado=suma (5,6);
+		//hola(); // imprime hola 4 veces
+//		int resultado=suma (5,6); // suma 2 umeros pedidos
 //		System.out.println(resultado);
 //		
 //		
 		
-		//adios();
+		//adios();// imprime adios 4 veces
 		
 		
 		
 //	}
+// metodo suma en el que sumas 2 numeros
 //	private static int suma(int a , int b) {
 //		return a+b;
 //	}
+//metodo suma en el que dices adios 4 veces
 //	private static void adios() {
 //		System.out.println("adios");
 //		System.out.println("adios");
 //		System.out.println("adios");
 //		System.out.println("adios");
 //	}
-
+//metodo suma en el que dices hola 4 veces
 //	private static void hola() {
 //		System.out.println("hola");
 //		System.out.println("hola");
