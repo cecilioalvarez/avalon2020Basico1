93b3a3f68e5c94a5e1aabecb49c3a11799d6d52b
Formacion <Formacion@5CD9494ZLTC>
2020-03-11 12:51:22 +0100

    NUevos ejercicios con arrays

---
+++ 001Basico/src/es/avalon/Principal23arrayalreves.java
@@ -0,0 +1,29 @@
+package es.avalon;
+
+public class Principal23arrayalreves {
+
+	public static void main(String[] args) {
+
+		// Sacar el array al reves
+
+		int[] vector = new int[] { 1, 2, 3, 4, 5 }; // introduzco el array
+
+		int[] vectorordenado = reordenar(vector);
+		imprimir(vectorordenado);
+	}
+
+	private static int[] reordenar(int[] vector) {
+		int[] nueva = new int[vector.length];
+
+		for (int i = 0; i < vector.length; i++) {
+			nueva[vector.length - i - 1] = vector[i];
+		}
+		return nueva;
+	}
+
+	private static void imprimir(int[] vector) { // con esta funcion imprimo el array
+		for (int i = 0; i < vector.length; i++) {
+			System.out.println(vector[i]);
+		}
+	}
+}
