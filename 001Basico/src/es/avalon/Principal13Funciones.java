f6333fcc2c8d4d4a0a214a4806a3ed62cdfc92ec
Formacion <Formacion@DESKTOP-E5B1ANP>
2020-03-11 09:32:17 +0000

    Principal13

---
+++ 001Basico/src/es/avalon/Principal13Funciones.java
@@ -0,0 +1,51 @@
+package es.avalon;
+
+public class Principal13Funciones {
+	
+	
+	private static void lista (int a) {
+		
+		int[] Array3 = new int[a];
+		for (int i=0; i<a; i++)
+		{
+			Array3[i]=i;
+			System.out.println(Array3[i]);
+		}
+		
+		
+	}
+	
+	
+	
+	public static void main(String[] args) {
+		
+//		
+		lista(3);
+		
+//		hola();
+//		Adios();		
+//		hola();
+//
+	}
+//
+//	private static void Adios() {
+//		System.out.println("Adios");
+//		System.out.println("Adios");
+//		System.out.println("Adios");
+//		System.out.println("Adios");
+//	}
+//
+//	private static void hola() {
+//		System.out.println("hola");
+//		System.out.println("hola");
+//		System.out.println("hola");
+//		System.out.println("hola");
+//	}
+
+	
+
+	
+
+	}
+	
+
