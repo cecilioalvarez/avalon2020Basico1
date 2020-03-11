d5ec80a0f3d99b630d9a6672112791592960d7dc
cecilioalvarezcaules <cecilioalvarezcaules@10.39.1.65>
2020-03-11 13:48:12 +0100

    inversa

---
+++ 001Basico/src/es/avalon/Principal15Inverso.java
@@ -0,0 +1,37 @@
+package es.avalon;
+
+public class Principal15Inverso {
+
+	public static void main(String[] args) {
+		
+		int[] lista= new int[] {1,2,3};
+		
+		int[] inversa=inversa(lista);
+		
+		for (int i=0;i<inversa.length;i++) {
+			
+			System.out.println(inversa[i]);
+		}
+		
+		
+	}
+	
+	private static int[] inversa(int[] otra) {
+		
+		int[] nueva=new int[otra.length];
+		
+		for (int i=0;i<nueva.length;i++) {
+			
+			//System.out.println("iteracion:"+i);
+			//System.out.println("dato:"+otra[i]);
+			nueva[i]=otra[otra.length-i-1];
+			
+			
+			
+			
+		}
+		
+		return nueva;
+		
+	}
+}
