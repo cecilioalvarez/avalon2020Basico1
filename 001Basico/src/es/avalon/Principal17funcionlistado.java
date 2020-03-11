a80b052060c19e41c24cbebd0b24773728011c46
Formacion <Formacion@5CD9494ZMXC>
2020-03-11 10:22:37 +0100

    funciones 11 marzo

---
+++ 001Basico/src/es/avalon/Principal17funcionlistado.java
@@ -0,0 +1,21 @@
+package es.avalon;
+
+public class Principal17funcionlistado {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		// definimos una funcion que devuelva la lista de numeros hasta el numero introducido
+		
+		listado(20);
+	}
+
+	private static void listado(int x) {
+		
+		for(int i=0; i<x; i++) {
+			
+			System.out.println(i);
+		}
+	}
+
+}
