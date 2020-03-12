3bbc87a125012dc5647c48d7a97946183e241c1f
Formacion <Formacion@DESKTOP-E5B1ANP.technicolor.net>
2020-03-12 09:22:08 +0000

    Rectangulo

---
+++ 001Basico/src/es/avalon/clase1/PrincipalRect.java
@@ -0,0 +1,17 @@
+package es.avalon.clase1;
+
+public class PrincipalRect {
+
+	public static void main(String[] args) {
+		
+		Rectangulo r= new Rectangulo();
+		
+		r.lado1=2;
+		r.lado2=2;
+		System.out.println(r.area());
+		System.out.println(r.perimetro());
+		
+
+	}
+
+}
---
+++ 001Basico/src/es/avalon/clase1/Rectangulo.java
@@ -0,0 +1,14 @@
+package es.avalon.clase1;
+
+public class Rectangulo {
+
+	double lado1;
+	double lado2;
+	
+	double area() {
+		return lado1*lado2;
+	}
+	double perimetro() {
+		return lado1*2+lado2*2;
+	}
+}
