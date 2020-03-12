77363c4066bd7f731436236ee7533db6ac48ef4a
Formacion <Formacion@5CD9494ZJNC>
2020-03-12 09:58:39 +0100

    persona2

--- 001Basico/src/es/avalon/personaprincipal.java
+++ 001Basico/src/es/avalon/personaprincipal.java
@@ -5,15 +5,15 @@ public class personaprincipal {
 		persona p1= new persona();
 		p1.edad=55;
 		p1.nombre="Jesús";
-		Donut d2= new Donut();
-		d2.precio=2;
-		d2.sabor="fresa";
+//		Donut d2= new Donut();
+//		d2.precio=2;
+//		d2.sabor="fresa";
 		//el operador . me permite acceder a aceda a cada propiedad del objeto
 		System.out.println(p1.edad);
 		System.out.println(p1.nombre);
-		System.out.println(d2.precio);
-		System.out.println(d2.sabor);
-		
+//		System.out.println(d2.precio);
+//		System.out.println(d2.sabor);
+//		
 	}
 
 }
