58cf1c66d1ed2c71e56893ed54fb829002bbddcd
Razvan Pascal <razvanpascal@outlook.com>
2020-03-12 10:16:27 +0100

    Rectangulo

---
+++ 001Basico/src/es/avalon/clases1/Princiapal4.java
@@ -0,0 +1,17 @@
+package es.avalon.clases1;
+
+public class Princiapal4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Rectangulo r1 = new Rectangulo();
+		r1.lado1 = 2;
+		r1.lado2 = 5;
+		
+		System.out.println(r1.area());
+		System.out.println(r1.perimetro());
+
+	}
+
+}
---
+++ 001Basico/src/es/avalon/clases1/Rectangulo.java
@@ -0,0 +1,14 @@
+package es.avalon.clases1;
+
+public class Rectangulo {
+	double lado1;
+	double lado2;
+	
+	//funciones
+	double perimetro() {
+		return lado1*2 + lado2*2;
+	}
+	double area() {
+		return lado1*lado2;
+	}
+}
