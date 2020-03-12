b2d7f37865c6c31546ded962c912433b496de0c7
Formacion <Formacion@5CD9494ZLKC>
2020-03-12 13:56:48 +0100

    Final clase 20200312

--- 001Basico/src/es/avalon/Clases2/Tableta.java
+++ 001Basico/src/es/avalon/Clases2/Tableta.java
@@ -5,16 +5,22 @@ public class Tableta {
 	private String marca;
 	private double precio;
 	
-	public double getPrecioConIVA() {
-		return precio *1.21;
-	}
-	public double getPrecioConIVA(int porcentaje) {
-		return precio + (precio/100)*porcentaje;
-	}
-	
 	public Tableta(String marca, int precio) {
 		this.marca = marca;
 		this.precio = precio;
+	}
+	
+	public Tableta(int precio) {
+		this.marca= "Apple";
+		this.precio=precio;
+	}
+	
+	public double getPrecioConIVA() {
+		return precio *1.21;
+	}
+	
+	public double getPrecioConIVA(int porcentaje) {
+		return precio + (precio/100)*porcentaje;
 	}
 	
 	public String getMarca() {
--- 001Basico/src/es/avalon/Clases3/Principal2.java
+++ 001Basico/src/es/avalon/Clases3/Principal2.java
@@ -9,8 +9,10 @@ public class Principal2 {
 		Tableta tableta = new Tableta("Apple", 100);
 		System.out.println(tableta.getMarca());
 		System.out.println(tableta.getPrecio());
-		
 		System.out.println(tableta.getPrecioConIVA(10));
+
+		Tableta tabletaApple = new Tableta(150);
+		System.out.println(tabletaApple.getPrecio() + "  " + tableta.getMarca());
 	}
 
 }
