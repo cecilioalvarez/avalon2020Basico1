ea9a86d0de47ab70b3841f38c9200307b32f192e
Razvan Pascal <razvanpascal@outlook.com>
2020-03-12 11:27:52 +0100

    Telefono

---
+++ 001Basico/src/es/avalon/clases3/Principal.java
@@ -0,0 +1,13 @@
+package es.avalon.clases3;
+
+import es.avalon.clases2.Telefono;
+
+public class Principal {
+	public static void main(String[] args) {
+		
+		Telefono t = new Telefono();
+		t.numero = 123456789;
+		//no tenemos acceso al numero
+		System.out.println(t.numero);
+	}
+}
