b21277d3172e130e9c9264a912a2a26a2f43d41e
Formacion <Formacion@5CD9494ZJNC>
2020-03-12 13:47:19 +0100

    tableta

---
+++ 001Basico/src/es/clases1/tableta.java
@@ -0,0 +1,34 @@
+package es.clases1;
+
+public class tableta {
+private String marca;
+	public String getMarca() {
+	return marca;
+}
+public void setMarca(String marca) {
+	this.marca = marca;
+}
+public double getPrecio() {
+	return precio;
+}
+public void setPrecio(double precio) {
+	this.precio = precio;
+}
+// constructor
+	private double precio;
+	public tableta(String marca, double precio) {
+		super();
+		this.marca = marca;
+		this.precio = precio;
+	}
+	
+	public double getPrecioconIVA() {
+		return precio*1.21;
+	}
+	
+	public double getPrecioconIVA(int porcentaje) {
+		return precio -porcentaje * (precio/100);
+	}
+
+}
+//source-> geters setters
--- 001Basico/src/es/clases2/Principal.java
+++ 001Basico/src/es/clases2/Principal.java
@@ -2,24 +2,32 @@ package es.clases2;
 import es.clases1.telefono;
 import es.clases1.alumnos;
 import es.clases1.persona;
-
+import es.clases1.*;
 public class Principal {
-	public static void main (String[]arg) {
-		alumnos a= new alumnos("pepe", 7);
-		alumnos a2= new alumnos("ana", 5);
-		System.out.println(a);
-		System.out.println(a2);
-		
-//	telefono t= new telefono();
-////		t.numero=123456789;
-////		System.out.println(t.numero);
-//		t.setNumero(123456799);
-//		System.out.println(t.getNumero());
-//		alumnos a= new alumnos();
-//		a.setNota(8);
-//		System.out.println(a.getNota());
+public static void main (String[]arg) {
+//		alumnos a= new alumnos("pepe", 7);
+//		alumnos a2= new alumnos("ana", 5);
+//		System.out.println(a.getnota());
+//		System.out.println(a2.getnota());
 //		
-		
+//		
+////	telefono t= new telefono();
+//////		t.numero=123456789;
+//////		System.out.println(t.numero);
+////		t.setNumero(123456799);
+////		System.out.println(t.getNumero());
+////		alumnos a= new alumnos();
+////		a.setNota(8);
+////		System.out.println(a.getNota());
+////		
+//		
+	tableta t= new tableta ("apple", 1000);
+	System.out.println(t.getMarca());
+	System.out.println(t.getPrecio());
+	System.out.println(t.getPrecioconIVA());
+	System.out.println(t.getPrecioconIVA(10));
+	}
+	
 	}
 
-}
+
