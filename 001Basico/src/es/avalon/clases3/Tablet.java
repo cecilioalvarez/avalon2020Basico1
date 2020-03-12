debc41036c8d97eedbb03af4559f629627ae9411
cursos <cursos@localhost>
2020-03-12 13:40:40 +0100

    revision overload

--- 001Basico/src/es/avalon/clases3/Tablet.java
+++ 001Basico/src/es/avalon/clases3/Tablet.java
@@ -1,38 +1,41 @@
 package es.avalon.clases3;
 
 public class Tablet {
+
 	private String marca;
 	private int precio;
-	
+
 	public String getMarca() {
 		return marca;
 	}
+
 	public void setMarca(String marca) {
 		this.marca = marca;
 	}
+
 	public int getPrecio() {
 		return precio;
-		
-		public double getPrecioConIva() {
-			return precio * 1.21;
-			
-		}
-		public double getPrecioConIva() {
-			return precio + porcentaje*(precio/100);
-			
-		}
+
 	}
+	//precio con iva mal las llaves
+	public double getPrecioConIva() {
+		return precio * 1.21;
+
+	}
+	//faltaba el parametro porcentaje
+	public double getPrecioConIva(int porcentaje) {
+		return precio + porcentaje * (precio / 100);
+
+	}
+
 	public void setPrecio(int precio) {
 		this.precio = precio;
 	}
+
 	public Tablet(String marca, int precio) {
 		super();
 		this.marca = marca;
 		this.precio = precio;
 	}
-	
-	
-	
-	
 
 }
