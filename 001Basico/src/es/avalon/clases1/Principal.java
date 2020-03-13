e804f9c3687f1f0662dca2ee9c41adbfdc6c2d20
cursos <cursos@localhost>
2020-03-13 13:54:28 +0100

    asumo cambios

--- 001Basico/src/es/avalon/clases1/Principal.java
+++ 001Basico/src/es/avalon/clases1/Principal.java
@@ -1,21 +1,5 @@
-<<<<<<< HEAD
-package es.avalon.clases1;
 
-public class Principal {
 
-	public static void main(String[] args) {
-		Donut d1= new Donut();
-		d1.precio=1;
-		d1.sabor="chocolate";
-		
-		// el operador . me permite acceder a cada propiedad del objeto
-		
-		System.out.println(d1.precio);
-		System.out.println(d1.sabor);
-	}
-
-}
-=======
 package es.avalon.clases1;
 
 public class Principal {
@@ -43,4 +27,4 @@ public class Principal {
 	}
 
 }
->>>>>>> branch 'master' of https://github.com/cecilioalvarez/avalon2020Basico1.git
+
