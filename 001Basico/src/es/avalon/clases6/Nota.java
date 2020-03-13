0f385f42717e8bca270e791fd40a170f0320b533
Formacion <Formacion@5CD9494ZKXC>
2020-03-13 13:03:33 +0100

    ejer

---
+++ 001Basico/src/es/avalon/clases6/Nota.java
@@ -0,0 +1,5 @@
+package es.avalon.clases6;
+
+public class Nota {
+
+}
---
+++ 001Basico/src/es/avalon/clases6/Principal.java
@@ -0,0 +1,42 @@
+package es.avalon.clases6;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		int nota1=5;
+		int nota2=7;
+		int nota3=9;
+		
+		String nombre1="pedro";
+		String nombre2="ana";
+		String nombre3="gema";
+	
+		double media= (nota1+nota2+nota3)/3;
+		
+		System.out.println(media);
+		
+		//sacar la persona que tiene la mayor nota
+		
+		if (nota1>nota2){
+			
+			if(nota1>nota3) {
+			
+		
+			
+			System.out.println(nombre1);
+		
+			}else if(nota2>nota3 && nota2>nota1) {
+				
+			
+	
+			System.out.println(nombre2);
+			}
+		}
+	}
+}
+		
+		
+
+	
+
+
