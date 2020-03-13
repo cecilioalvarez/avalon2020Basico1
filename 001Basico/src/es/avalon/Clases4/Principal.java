d32b4bf67a34f32d9fffdab97f847d963966a881
Formacion <Formacion@5CD9494ZLKC>
2020-03-13 10:28:16 +0100

    Ejercicio factura estatico

--- 001Basico/src/es/avalon/Clases4/Principal.java
+++ 001Basico/src/es/avalon/Clases4/Principal.java
@@ -4,12 +4,23 @@ public class Principal {
 
 	public static void main(String[] args) {
 
-		Libro libro = new Libro("Java");
-		System.out.println(libro.getTitulo());
-		Libro libro2 = new Libro("Java2");
-		System.out.println(libro2.getTitulo());
 		
-		System.out.println(Libro.getContador());
+//		Libro libro = new Libro("Java");
+//		System.out.println(libro.getTitulo());
+//		Libro libro2 = new Libro("Java2");
+//		System.out.println(libro2.getTitulo());
+//		Libro libro3 = new Libro("Java3");
+//		System.out.println(libro3.getTitulo());
+//		System.out.println(Libro.getContador());
+		
+		
+		Factura factura = new Factura(1,100);
+		System.out.println(factura.getNumero());
+		System.out.println(factura.getImporte());
+		System.out.println(Factura.calcularIVA(10));
+		
+		
+		
 	}
 
 }
