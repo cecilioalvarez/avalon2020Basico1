81f991cb4777e878df50c94a68d45d37c23bf2a8
Oliver <Oliver@MSI.station>
2020-03-13 12:01:12 +0100

    p3 repaso

--- 001Basico/src/es/avalon/clases5repaso/Principal3.java
+++ 001Basico/src/es/avalon/clases5repaso/Principal3.java
@@ -3,8 +3,36 @@ package es.avalon.clases5repaso;
 public class Principal3 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		// un trillon
+		int [] notas= new int[] {5,7,9};
+		String [] nombres= new String[] {"pedro","ana","gema"};
+		
+		/////////////////////////////////////////////////////
+		
+		int media=0;
+		int notaInicial=0;
+		int posicionMayor=0;
+		
+		
+		
+		
+		for(int i=0;i<notas.length;i++) {
+			
+			media=media+notas[i];
+			
+			if (notaInicial<notas[i]) {
+				
+				posicionMayor=i;
+				notaInicial=notas[i];
+			}
+		}
+		
+		
+		
+		System.out.println(media/notas.length);
+		System.out.println(nombres[posicionMayor]);
+		
 	}
 
 }
