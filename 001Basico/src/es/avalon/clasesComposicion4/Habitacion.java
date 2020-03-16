942a5060b8d28e00e88280196c788e4eb3487767
Formacion <Formacion@192.168.1.201>
2020-03-16 13:43:08 +0100

    luces

--- 001Basico/src/es/avalon/clasesComposicion4/Habitacion.java
+++ 001Basico/src/es/avalon/clasesComposicion4/Habitacion.java
@@ -3,43 +3,42 @@ package es.avalon.clasesComposicion4;
 public class Habitacion {
 
 	private String nombre;
-	
-	
-	private Bombilla bombilla1;
+
 	private Bombilla bombilla2;
-	
-	
-	
-	
+
+	private Bombilla bombilla1;
+
+	String getNombre1() {
+		return nombre;
+	}
+
 	public Bombilla getBombilla2() {
 		return bombilla2;
 	}
+
 	public void setBombilla2(Bombilla bombilla2) {
-		this.bombilla2 = bombilla2;
+		this.bombilla2 = bombilla2;	
 	}
+
 	public String getNombre() {
 		return nombre;
 	}
+
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
+
 	public Bombilla getBombilla() {
 		return bombilla1;
 	}
+
 	public void setBombilla(Bombilla bombilla) {
 		this.bombilla1 = bombilla;
 	}
+
 	public Habitacion(String nombre) {
 		super();
 		this.nombre = nombre;
 	}
-	
-	
-	
-	
-	
-	
 
-
-	
 }
---
+++ 001Basico/src/es/avalon/clasesComposicion5/PrincipalBombilla.java
@@ -0,0 +1,27 @@
+package es.avalon.clasesComposicion5;
+
+public class PrincipalBombilla {
+
+	public static void main(String[] args) {
+		Bombilla b1= new Bombilla (true);
+		
+		imprimirEstadoBombilla(b1);
+		b1.apagar();
+		imprimirEstadoBombilla(b1);
+		b1.encender();
+		imprimirEstadoBombilla(b1);
+	}
+
+	public static void imprimirEstadoBombilla(Bombilla b) {
+		if(b.estaEncendida()) {
+			
+		System.out.println("encendida");
+		}else {
+			System.out.println("apagada");
+			
+		}
+	}
+	
+	
+	
+}
