9d40b154096d3a1035297fd332591ec5de14b0a3
Oliver <Oliver@MSI.station>
2020-03-16 11:16:00 +0100

    DNI

--- 001Basico/src/es/avalon/clases6composicion2/Dni.java
+++ 001Basico/src/es/avalon/clases6composicion2/Dni.java
@@ -2,4 +2,41 @@ package es.avalon.clases6composicion2;
 
 public class Dni {
 
+	private String numero;
+	private String fecha;
+	
+	private Persona persona;
+	
+	
+	public Persona getPersona() {
+		return persona;
+	}
+
+	public void setPersona(Persona persona) {
+		this.persona = persona;
+	}
+
+	public String getFecha() {
+		return fecha;
+	}
+
+	public void setFecha(String fecha) {
+		this.fecha = fecha;
+	}
+
+	public String getNumero() {
+		return numero;
+	}
+
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+
+	public Dni(String numero, String fecha, Persona persona) {
+		super();
+		this.numero = numero;
+		this.fecha = fecha;
+		this.persona = persona;
+	}
+	
 }
--- 001Basico/src/es/avalon/clases6composicion2/Principal.java
+++ 001Basico/src/es/avalon/clases6composicion2/Principal.java
@@ -2,4 +2,53 @@ package es.avalon.clases6composicion2;
 
 public class Principal {
 
+	public static void main(String[] args) {
+		
+		
+		Persona p= new Persona("gema");
+		Dni dni= new Dni("1","hoy",p);
+		p.setDni(dni);
+		
+		
+		Persona p2= new Persona("cecilio");
+		
+		Dni dni2= new Dni("1","hoy",p2);
+		
+		System.out.println(dni2.getNumero());
+		// es el objeto dni y accedo al numero
+		//dni2.getNumero()
+		
+		// es el objto dni accedo al objeto persona que tiene
+		dni2.getPersona().getNombre();
+		
+		Persona refPersona=dni2.getPersona();
+		System.out.println(refPersona.getNombre());
+//		
+//		p2.setDni(dni2);
+//		
+//		Dni[] lista= new Dni[] {p.getDni(),p2.getDni()};
+//		
+//		
+////		System.out.println(dni.getPersona().getNombre());
+////		System.out.println(p.getNombre());
+////		
+//		imprimirNombresDeDni(lista);
+	}
+
+	
+	
+	public static void imprimirNombresDeDni (Dni[] lista) {
+		
+		// en este trozo no hay forma de acceder directamente
+		// a la lista de personas porque no la recibo
+		//recibo una lista de dnis
+		//claro como los dnis estan relacionados con 
+		//las personas puedo acceder a ellas.
+		
+		for (int i=0;i<lista.length;i++) {
+			
+			System.out.println(lista[i].getPersona().getNombre());
+		}
+	}
+	
 }
