63d3d07d42e3cdd25009844302d7c035696d3be0
cursos <cursos@localhost>
2020-03-16 09:48:03 +0100

    comentarios

--- 001Basico/src/Clase6Composicion/Principal.java
+++ 001Basico/src/Clase6Composicion/Principal.java
@@ -8,7 +8,11 @@ public class Principal {
 		HistorialMedico h1=new HistorialMedico("Catarro",p1);
 		p1.setHistorial(h1);
 		
+		// en esta linea estas imprimiendo el historial que es
+		//un objeto y por lo tanto sale su referencia a memoria
+		// lo que necesitas es imprimir la enfermedad
 		System.out.println(p1.getHistorial());
+		//y es aqui donde la imprimes correctamente
 		System.out.println(p1.getHistorial().getEnfermedad());
 	}
 
