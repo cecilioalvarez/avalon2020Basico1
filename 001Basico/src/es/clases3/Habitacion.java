bce1e9c327f1072c912b564bf699904747aa2172
Formacion <Formacion@5CD9494ZJNC>
2020-03-16 11:32:05 +0100

    bombillas

---
+++ 001Basico/src/es/clases3/Habitacion.java
@@ -0,0 +1,13 @@
+package es.clases3;
+
+public class Habitacion {
+	private Bombilla bombilla;
+
+	public Bombilla getBombilla() {
+		return bombilla;
+	}
+
+	public void setBombilla(Bombilla bombilla) {
+		this.bombilla = bombilla;
+	}
+}
--- 001Basico/src/es/clases3/Principal1.java
+++ 001Basico/src/es/clases3/Principal1.java
@@ -14,25 +14,38 @@ public class Principal1 {
 //		System.out.println(p1.getHistorial().getEnfermedad());
 		
 		
-		//crea persona
-				Persona p1= new Persona ("Pedro");
-				//asigna el historial al paciente 
-				
-				//crea dni
-				DNI d1= new DNI(p1, 123456789, "13/2/89");
-				
-				
-				//que estoy haciendo aqui? 
-				/*Pregutnas:
-				 * -a que metodo estoy refernciadno, todo!
-				 * -por que solo saco el nombre, si quiero sacar todo
-				 * -
-				 */
-				System.out.println(d1.getPersona().getNombre() );
-				System.out.println(d1.getNumero() );
-				System.out.println(d1.getFechaCaducidad());
-				//accedo a la relacion y a la propiedad de historial de enfermedad
-				//System.out.println(p1.getHistorial().getEnfermedad());
+//		//crea persona
+//				Persona p1= new Persona ("Pedro");
+//				//asigna el historial al paciente 
+//				
+//				//crea dni
+//				DNI d1= new DNI(p1, 123456789, "13/2/89");
+//				
+//				
+//				//que estoy haciendo aqui? 
+//				/*Pregutnas:
+//				 * -a que metodo estoy refernciadno, todo!
+//				 * -por que solo saco el nombre, si quiero sacar todo
+//				 * -
+//				 */
+//				System.out.println(d1.getPersona().getNombre() );
+//				System.out.println(d1.getNumero() );
+//				System.out.println(d1.getFechaCaducidad());
+//				//accedo a la relacion y a la propiedad de historial de enfermedad
+//				//System.out.println(p1.getHistorial().getEnfermedad());
+		
+		Bombilla b1= new Bombilla (true);
+		System.out.println(b1.isBombilla());
+		b1.apagado();
+		System.out.println(b1.isBombilla());
+		public static void imprimirEstadoBombilla(Bombilla b) {
+			if (b.isBombilla()==true ) {
+				System.out.println("encendido");
+			}else {
+				System.out.println("apagada");
+			}
+		}
+		
 	}
 
 }
