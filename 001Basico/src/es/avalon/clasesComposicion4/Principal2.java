23d032ab8526a3eb2f998e93f603239b9bd22bfa
cursos <cursos@localhost>
2020-03-16 12:43:21 +0100

    coregido

--- 001Basico/src/es/avalon/clasesComposicion4/Principal2.java
+++ 001Basico/src/es/avalon/clasesComposicion4/Principal2.java
@@ -3,13 +3,13 @@ package es.avalon.clasesComposicion4;
 public class Principal2 {
 
 	public static void main(String[] args) {
-		Habitacion h= new Habitacion();
+		Habitacion h= new Habitacion("habitacion1");
 		Bombilla b1 = new Bombilla(true);
-		Bombilla b2 = new Bombilla(False);
+		Bombilla b2 = new Bombilla(false);
 		h.setBombilla2(b2);
 		
-		h.setBombilla1(b1);
-		System.out.println(h.getBombilla1().estaEncendida());
+		h.setBombilla(b1);
+		System.out.println(h.getBombilla().estaEncendida());
 		System.out.println(h.getBombilla2().estaEncendida());
 	}
 
