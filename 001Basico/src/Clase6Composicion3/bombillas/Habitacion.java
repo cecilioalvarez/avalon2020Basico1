70c011c2faa0d858d2bdc70b05382b81b495ecfe
Formacion <Formacion@DESKTOP-E5B1ANP.technicolor.net>
2020-03-16 13:49:53 +0000

    errores

---
+++ 001Basico/src/Clase6Composicion3/bombillas/Habitacion.java
@@ -0,0 +1,29 @@
+package Clase6Composicion3.bombillas;
+
+import java.util.ArrayList;
+
+//
+public class Habitacion {
+	private String nombre;
+	private ArrayList<Bombilla> bombillas=new ArrayList<Bombilla>();
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public ArrayList<Bombilla> getBombillas() {
+		return bombillas;
+	}
+	public void setBombillas(ArrayList<Bombilla> bombillas) {
+		this.bombillas = bombillas;
+	}
+	public Habitacion() {
+		super();
+		
+	}
+	
+
+
+
+}
---
+++ 001Basico/src/Clase6Composicion3/bombillasArray/Habitacion.java
@@ -0,0 +1,34 @@
+package Clase6Composicion3.bombillasArray;
+
+public class Habitacion {
+	private String nombre;
+	private Bombilla bombilla;
+	private Bombilla bombilla2;
+	
+	
+	public Bombilla getBombilla2() {
+		return bombilla2;
+	}
+	public void setBombilla2(Bombilla bombilla2) {
+		this.bombilla2 = bombilla2;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public Bombilla getBombilla() {
+		return bombilla;
+	}
+	public void setBombilla(Bombilla bombilla) {
+		this.bombilla = bombilla;
+	}
+	public Habitacion() {
+		super();
+
+	}
+
+
+
+}
