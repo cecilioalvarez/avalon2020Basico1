14c5a71d174c6b0dcf916c9df27329cfd976b715
cursos <cursos@localhost>
2020-03-16 13:26:45 +0100

    add bombilla en la habitacion

--- 001Basico/src/es/clases3/Habitacion.java
+++ 001Basico/src/es/clases3/Habitacion.java
@@ -36,27 +36,36 @@ public class Habitacion {
 //	public void setBombilla2(Bombilla bombilla2) {
 //		this.bombilla2 = bombilla2;
 //	}
-	//nuevo ejercicio
-	//************************************************
+	// nuevo ejercicio
+	// ************************************************
 
 	private String nombre;
-	private ArrayList<Bombilla> bombillas=new ArrayList<Bombilla>();
-	
+	private ArrayList<Bombilla> bombillas = new ArrayList<Bombilla>();
+
 	public String getNombre() {
 		return nombre;
 	}
+
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
+
 	public ArrayList<Bombilla> getBombillas() {
 		return bombillas;
 	}
+
 	public void setBombillas(ArrayList<Bombilla> bombillas) {
 		this.bombillas = bombillas;
 	}
+
 	public Habitacion() {
 		super();
-		
+
 	}
-}
+
+	public void addBombilla(Bombilla b) {
+
+		this.bombillas.add(b);
+	}
+
 }
