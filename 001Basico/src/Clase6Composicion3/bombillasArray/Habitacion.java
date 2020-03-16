2a492ead0fa13c86f6863a837c06cd07e73b4656
cursos <cursos@localhost>
2020-03-16 14:03:53 +0100

    nuevos

--- 001Basico/src/Clase6Composicion3/bombillasArray/Habitacion.java
+++ 001Basico/src/Clase6Composicion3/bombillasArray/Habitacion.java
@@ -1,34 +1,44 @@
 package Clase6Composicion3.bombillasArray;
 
+import Clase6Composicion3.bombillas.Bombilla;
+
 public class Habitacion {
 	private String nombre;
 	private Bombilla bombilla;
 	private Bombilla bombilla2;
-	
-	
+
 	public Bombilla getBombilla2() {
 		return bombilla2;
 	}
+
 	public void setBombilla2(Bombilla bombilla2) {
 		this.bombilla2 = bombilla2;
 	}
+
 	public String getNombre() {
 		return nombre;
 	}
+
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
+
 	public Bombilla getBombilla() {
 		return bombilla;
 	}
+
 	public void setBombilla(Bombilla bombilla) {
 		this.bombilla = bombilla;
 	}
+
 	public Habitacion() {
 		super();
 
 	}
 
+	public void addBombilla(Bombilla b) {
 
+		this.bombillas.add(b);
+	}
 
 }
