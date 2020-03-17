e94972f9ed8027102b00ad952261ce4c85c3009a
Oliver <Oliver@MSI.station>
2020-03-17 10:07:33 +0100

    herencia

---
+++ 001Basico/src/es/avalon/clases8/herencia/Telefono.java
@@ -0,0 +1,31 @@
+package es.avalon.clases8.herencia;
+
+public class Telefono {
+	private int numero;
+	private String marca;
+
+	public int getNumero() {
+		return numero;
+	}
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+	public String getMarca() {
+		return marca;
+	}
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+	
+	public Telefono(int numero, String marca) {
+		super();
+		this.numero = numero;
+		this.marca = marca;
+	}
+	
+	public void llamar(int otroNumero) {
+		
+		System.out.println("desde el numero"+ getNumero()+"llamamos a "+ otroNumero);
+	}
+	
+}
