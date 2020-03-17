7766a031e04a8ad7f029c610a0f2e111ee70eae3
Formacion <Formacion@192.168.1.201>
2020-03-17 12:14:12 +0100

    deportista

--- 001Basico/src/es/avalon/clases9Polimorfismo/Principal2.java
+++ 001Basico/src/es/avalon/clases9Polimorfismo/Principal2.java
@@ -5,20 +5,41 @@ import java.util.ArrayList;
 public class Principal2 {
 
 	public static void main(String[] args) {
-		
-		ArrayList<Integer> deportista = new ArrayList<Integer>();
-		
-		deportista.add("juan");
-		deportista.add("Pedro");
-		deportista.add("Ivan");
-		
-		for(Deportista d: lista) {
-			System.out.println(d.getNombre());
+
+		Deportista d1 = new Deportista("pedro", 20, "tenis");
+
+		Deportista d2 = new Deportista("ana", 25, "futbol");
+
+		Deportista d3 = new Deportista("victor", 45, "tenis");
+
+		Deportista d4 = new Deportista("paco", 20, "baloncesto");
+
+		ArrayList<Deportista> lista = new ArrayList<Deportista>();
+
+		lista.add(d1);
+		lista.add(d2);
+		lista.add(d3);
+		lista.add(d4);
+
+		for (Deportista undeportista : lista) {
+			System.out.println(undeportista.getNombre());
+		}
+
+		ArrayList<Persona> lista2 = new ArrayList<Persona>();
+
+		lista2.add(d1);
+		lista2.add(d2);
+		lista2.add(d3);
+		lista2.add(d4);
+
+		for (Persona undeportista : lista2) {
+			System.out.println(undeportista.getNombre());
 		}
 		
 		
 		
-
+		
+		
 	}
 
 }
