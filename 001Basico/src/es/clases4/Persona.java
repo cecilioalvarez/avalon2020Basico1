6b30fe859e1eb9f7e04ddc96978c78b8e96fc867
Formacion <Formacion@5CD9494ZJNC>
2020-03-17 13:56:44 +0100

    final 17/3

---
+++ 001Basico/src/es/clases4/Persona.java
@@ -0,0 +1,31 @@
+package es.clases4;
+
+public class Persona {
+	private String nombre;
+	private int edad;
+
+	
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public int getEdad() {
+		return edad;
+	}
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+	
+	public Persona(String nombre, int edad) {
+		super();
+		this.nombre = nombre;
+		this.edad = edad;
+	}
+	
+	public void andar(){
+		System.out.println(getNombre() +" va andando a 5km/h");
+	}
+	
+}
