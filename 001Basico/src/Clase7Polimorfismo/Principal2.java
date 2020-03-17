352e0937b9eb805fe52d027d1f8c060b21b638dc
Formacion <Formacion@DESKTOP-E5B1ANP.technicolor.net>
2020-03-17 12:09:19 +0000

    a

--- 001Basico/src/Clase7Polimorfismo/Principal2.java
+++ 001Basico/src/Clase7Polimorfismo/Principal2.java
@@ -3,14 +3,19 @@ import java.util.ArrayList;
 public class Principal2 {
 
 	public static void main(String[] args) {
+		Deportista d1=new Deportista("Juan",30,"natacion");
+		Deportista d2=new Deportista("Jose",31,"padel");
+		Deportista d3=new Deportista("Maria",32,"tenis");
+		Deportista d4=new Deportista("Ines",33,"futbol");
+		
 		ArrayList<Deportista> lista=new ArrayList<Deportista>();
-		lista.add("Juan",30,"natacion");
-		lista.add("Jose",31,"padel");
-		lista.add("Maria",32,"tenis");
-		lista.add("Ines",33,"futbol");
+		lista.add(d1);
+		lista.add(d2);
+		lista.add(d3);
+		lista.add(d4);
 
-		for(Integer i:lista) {
-			System.out.println();
+		for(Deportista undeportista:lista) {
+			System.out.println(undeportista.getDeporte());
 			
 		}
 	}
