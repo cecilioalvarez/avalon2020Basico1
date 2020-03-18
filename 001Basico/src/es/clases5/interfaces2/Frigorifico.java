2dd1b15cd8a17ded249579e74b68b16de5faf996
cursos <cursos@localhost>
2020-03-18 13:25:32 +0100

    correcion

--- 001Basico/src/es/clases5/interfaces2/Frigorifico.java
+++ 001Basico/src/es/clases5/interfaces2/Frigorifico.java
@@ -1,36 +1,25 @@
 package es.clases5.interfaces2;
 
-public class Frigorifico implements ElementoCasa{
-private boolean encender;
+public class Frigorifico implements ElementoCasa {
 
-
-
-@Override
-public void interruptor() {
-	// TODO Auto-generated method stub
-	if(isEncender()) {
-		encender=false;		
-		}else {
-	encender= true;
-		}
-		System.out.println("frigo estado:"+ isEncender());
-	
-}
-
-
-//@Override
-public boolean isEncender() {
-	// TODO Auto-generated method stub
-	return false;
-}
-
-//public void setEncernder(boolean encernder) {
-//	this.encender = encernder;
-//}
+	private boolean encender;
 
 	@Override
-	
-	
-	
+	public void interruptor() {
+		// TODO Auto-generated method stub
+		if (isEncender()) {
+			encender = false;
+		} else {
+			encender = true;
+		}
+		System.out.println("frigo estado:" + isEncender());
+
+	}
+
+	@Override
+	public boolean isEncender() {
+		// TODO Auto-generated method stub
+		return this.encender;
+	}
 
 }
