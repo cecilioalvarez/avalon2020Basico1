9603d774f81e04ac08aa38461072a0d7121d6823
Formacion <Formacion@5CD9494ZJNC>
2020-03-18 13:45:25 +0100

    casa rara

--- 001Basico/src/es/clases5/interfaces2/Casa.java
+++ 001Basico/src/es/clases5/interfaces2/Casa.java
@@ -1,5 +1,16 @@
 package es.clases5.interfaces2;
 
+import java.util.ArrayList;
 public interface Casa {
+	private ArrayList<ElementoCasa> elemento= new ArrayList<ElementoCasa>();
+	
+
+	
+	public void addElemento(ElementoCasa e) {
+		this.elemento.add(e);
+	}
+	
+	
+	
 
 }
