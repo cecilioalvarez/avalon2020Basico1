e820d890816c8c5de9453734387562ddc89efe28
Formacion <Formacion@5CD9494ZMXC>
2020-03-20 13:31:39 +0100

    error

---
+++ 001Basico/src/es/avalon/Principal.java
@@ -0,0 +1,34 @@
+package es.avalon;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Connection conexion;
+		String url="jdbc:mysql://localhost:3306/biblioteca";
+		String usuario="root";
+		String clave="";
+		String consulta="insert into Libros (isbn,titulo,autor,precio,categoria) values ('5','net','Juan',20,'web)";
+		
+		try {
+			conexion=DriverManager.getConnection(url,usuario,clave);
+			
+			// sentencia SQL por lo tanto
+			// a la conexion que nos cree una opcion de ejecutar una sentencia
+			Statement sentencia=conexion.createStatement();
+			sentencia.execute(consulta);
+			
+			System.out.println(conexion);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
