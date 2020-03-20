3e9eb608cea5f9dc9028f83250abb540aded7dfa
cursos <cursos@localhost>
2020-03-20 14:06:51 +0100

    tema idioma

--- 001Basico/src/es/avalon/jdbc/Principal.java
+++ 001Basico/src/es/avalon/jdbc/Principal.java
@@ -9,7 +9,7 @@ public class Principal {
 
 	public static void main(String[] args) {
 		Connection conexion;
-		String url = "jdbc:mysql://localhost:3306/biblioteca";
+		String url = "jdbc:mysql://localhost:3306/biblioteca?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
 		String usuario = "root";
 		String clave = "";
 		String consulta = "insert into Libros (isbn,titulo,autor,precio,categoria) values ('8','net','juan',20,'web')";
