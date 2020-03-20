ef45b54a688ca2cc36c217eb61090a918e107978
cursos <cursos@localhost>
2020-03-20 13:31:43 +0100

    jdbc1

--- 001Basico/src/es/avalon/jdbc/Principal.java
+++ 001Basico/src/es/avalon/jdbc/Principal.java
@@ -1,8 +1,35 @@
 package es.avalon.jdbc;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class Principal {
 
 	public static void main(String[] args) {
+		
+		Connection conexion;
+		String url="jdbc:mysql://localhost:3306/biblioteca";
+		String usuario="root";
+		String clave="";
+		String consulta="insert into Libros (isbn,titulo,autor,precio,categoria) values ('5','net','juan',20,'web')";
+		
+		try {
+			conexion=DriverManager.getConnection(url,usuario,clave);
+			
+			//sentencia SQL por lo tanto
+			// a la conexion que nos cree una opcion de ejecutar una sentencia
+			Statement sentencia=conexion.createStatement();
+			sentencia.execute(consulta);
+			
+			
+			System.out.println(conexion);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 		
 		
 
