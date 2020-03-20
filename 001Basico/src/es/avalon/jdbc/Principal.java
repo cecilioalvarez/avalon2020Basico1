eb79daad14cfbc2c71cff16562d96ea38ee697f0
Formacion <Formacion@5CD9494ZLKC>
2020-03-20 13:23:13 +0100

    Conexion creada y añadida lib mysql.jar

--- 001Basico/src/es/avalon/jdbc/Principal.java
+++ 001Basico/src/es/avalon/jdbc/Principal.java
@@ -1,10 +1,24 @@
 package es.avalon.jdbc;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Principal {
 
 	public static void main(String[] args) {
 
-		
+		Connection conexion;
+		String url = "jdbc:mysql://localhost:3306/biblioteca";
+		String usuario = "root";
+		String clave = "";
+
+		try {
+			conexion = DriverManager.getConnection(url, usuario, clave);
+			System.out.println(conexion);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
