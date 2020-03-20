a8282959e1c176082b2dbdf384b82e41670ada56
Formacion <Formacion@5CD9494ZLKC>
2020-03-20 13:39:38 +0100

    consulta SQL

--- 001Basico/src/es/avalon/jdbc/Principal.java
+++ 001Basico/src/es/avalon/jdbc/Principal.java
@@ -3,6 +3,7 @@ package es.avalon.jdbc;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Principal {
 
@@ -12,9 +13,15 @@ public class Principal {
 		String url = "jdbc:mysql://localhost:3306/biblioteca";
 		String usuario = "root";
 		String clave = "";
+		String consulta="insert into Libros(isbn,titulo,autor,precio,categoria) values ('5','net','juan',20,'web')";
 
 		try {
 			conexion = DriverManager.getConnection(url, usuario, clave);
+			Statement sentencia = conexion.createStatement();
+			sentencia.execute(consulta);
+			
+			
+			
 			System.out.println(conexion);
 		} catch (SQLException e) {
 			e.printStackTrace();
