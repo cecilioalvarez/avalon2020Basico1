4bb54b453aecd4da21e7830403525bc770d10190
Formacion <Formacion@DESKTOP-E5B1ANP.technicolor.net>
2020-03-23 10:34:16 +0000

    a

--- 001Basico/src/es/avalon/jdbc/Principal3.java
+++ 001Basico/src/es/avalon/jdbc/Principal3.java
@@ -2,26 +2,31 @@ package es.avalon.jdbc;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+
 import java.sql.SQLException;
 import java.sql.Statement;
 
-public class Principal {
+public class Principal3 {
 
 	public static void main(String[] args) {
 		Connection conexion;
 		String url ="jdbc:mysql://localhost/biblioteca?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
 		String usuario = "root";
 		String clave = "";
-		String consulta = "insert into Libros (isbn,titulo,autor,precio,categoria) values ('8','net','juan',20,'web')";
+		String consulta = "delete from Libros where autor='juan'";
 
 		try {
 			conexion = DriverManager.getConnection(url, usuario, clave);
 			Statement sentencia = conexion.createStatement();
 			sentencia.execute(consulta);
-			System.out.println(conexion);
+			
+			
+			
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
 	}
 
 }
+
+
--- 001Basico/src/es/avalon/jdbc/Principal5.java
+++ 001Basico/src/es/avalon/jdbc/Principal5.java
@@ -4,14 +4,38 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Scanner;
 
-public class Principal {
+public class Principal5 {
 
 	public static void main(String[] args) {
+		
 		Connection conexion;
 		String url ="jdbc:mysql://localhost/biblioteca?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
 		String usuario = "root";
 		String clave = "";
+		
+		
+		Scanner sc=new Scanner(System.in);
+		
+		System.out.print("introduce isbn: ");
+		String isbn=sc.nextLine();
+		System.out.print("introduce titulo: ");
+		String titulo=sc.nextLine();
+		System.out.print("introduce autor: ");
+		String autor=sc.nextLine();
+		System.out.print("introduce precio: ");
+		String precio=sc.nextLine();
+		System.out.print("introduce categoria: ");
+		String categoria=sc.nextLine();
+		
+		System.out.println(isbn);
+		System.out.println(titulo);
+		System.out.println(autor);
+		System.out.println(precio);
+		System.out.println(categoria);
+		
+
 		String consulta = "insert into Libros (isbn,titulo,autor,precio,categoria) values ('8','net','juan',20,'web')";
 
 		try {
@@ -22,6 +46,7 @@ public class Principal {
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
+	
 	}
 
 }
