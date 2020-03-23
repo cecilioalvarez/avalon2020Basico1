b42cbd96a5c57660c07794b338e3cffd327ee993
Oliver <Oliver@MSI>
2020-03-23 10:56:13 +0100

    colocado

--- 001Basico/src/es/avalon/jdbc/principal5.java
+++ 001Basico/src/es/avalon/jdbc/principal5.java
@@ -10,49 +10,43 @@ public class principal5 {
 
 	public static void main(String[] args) {
 		System.out.print("Introduce isbn:  ");
-		Scanner sc=new Scanner(System.in);
-		String isbn=sc.nextLine();
+		Scanner sc = new Scanner(System.in);
+		String isbn = sc.nextLine();
 		System.out.print("Introduce titulo:  ");
-		String titulo=sc.nextLine();
+		String titulo = sc.nextLine();
 		System.out.print("Introduce autor:  ");
-		String autor=sc.nextLine();
+		String autor = sc.nextLine();
 		System.out.print("Introduce precio:  ");
-		int precio=Integer.parseInt(sc.nextLine());
+		int precio = Integer.parseInt(sc.nextLine());
 		System.out.print("Introduce categoria:  ");
-		String categoria=sc.nextLine();
-		
+		String categoria = sc.nextLine();
 
-  
 		Connection conexion;
-		String url="jdbc:mysql://localhost:3306/biblioteca";
-		String usuario="root";
-		String clave="";
-		
+		String url = "jdbc:mysql://localhost:3306/biblioteca";
+		String usuario = "root";
+		String clave = "";
+
 		/*
-		System.out.println(isbn);
-		System.out.println(titulo);
-		System.out.println(autor);
-		System.out.println(precio);
-		System.out.println(categoria);
-		*/
-		String consulta= "insert into Libros (isbn,titulo,autor,precio,categoria) values";
-		consulta=consulta+"('"+isbn+"','"+titulo+"', '"+autor+"','"+precio+"', '"+categoria+"')";
+		 * System.out.println(isbn); 
+		 * System.out.println(titulo);
+		 * System.out.println(autor); 
+		 * System.out.println(precio);
+		 * System.out.println(categoria);
+		 */
+		String consulta = "insert into Libros (isbn,titulo,autor,precio,categoria) values";
+		consulta = consulta + "('" + isbn + "','" + titulo + "', '" + autor + "','" + precio + "', '" + categoria
+				+ "')";
 		System.out.println(consulta);
-		
-		
+
 		try {
-			conexion=DriverManager.getConnection(url,usuario,clave);
-			Statement sentencia= conexion.createStatement();
+			conexion = DriverManager.getConnection(url, usuario, clave);
+			Statement sentencia = conexion.createStatement();
 			sentencia.execute(consulta);
 			System.out.println(conexion);
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
-		
-		
-		
 
 	}
 
