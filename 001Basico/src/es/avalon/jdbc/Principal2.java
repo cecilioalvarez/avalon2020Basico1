3595634e995f72575423fe8434017ebb9c2d36ce
Oliver <Oliver@MSI>
2020-03-23 09:48:51 +0100

    Principal2

---
+++ 001Basico/src/es/avalon/jdbc/Principal2.java
@@ -0,0 +1,41 @@
+package es.avalon.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Principal2 {
+
+	public static void main(String[] args) {
+		Connection conexion;
+		
+		String url="jdbc:mysql://localhost:3306/biblioteca";
+		String usuario="root";
+		String clave="";
+		String consulta="select * from Libros";
+		
+		try {
+			conexion=DriverManager.getConnection(url,usuario,clave);
+			Statement sentencia= conexion.createStatement();
+			ResultSet rs= sentencia.executeQuery(consulta);
+			
+			while (rs.next()){
+				System.out.print(rs.getString("isbn"));
+				System.out.print(rs.getString("titulo"));
+				System.out.print(rs.getString("autor"));
+				System.out.print(rs.getInt("precio"));
+				System.out.print(rs.getString("categoria"));
+				System.out.println();
+				
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+
+	}
+
+}
